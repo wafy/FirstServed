a7049bdceda2ef3884126f27d38b75e2f4665b39
최선혁 <wafy75@gmail.com>
2022-07-11 13:27:09 +0900

    프로젝트 초기 셋팅

---
+++ src/test/java/com/event/firstserved/FirstServedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.event.firstserved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstServedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
