925f3876dcfe64b5737b2a6974c4bdc293240f4d
최선혁 <wafy75@gmail.com>
2022-07-14 12:49:15 +0900

    조회 상세 단위테스트 추가

--- src/test/java/com/event/reward/core/query/RewardPageSearcherTest.java
+++ src/test/java/com/event/reward/core/query/RewardPageSearcherTest.java
@@ -1,10 +1,7 @@
 package com.event.reward.core.query;
 
 import com.event.reward.core.Reward;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
@@ -12,6 +9,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -65,6 +64,52 @@ class RewardPageSearcherTest extends RewardCrudSupplier {
                     Pageable pageable = PageRequest.of(0, 10, Sort.by("created").ascending());
                     Page<Reward> rewardList = getRewardPageSearcher().find(givenCreatedDate.minusDays(3), givenCreatedDate, pageable);
                     assertEquals(givenCreatedDateTime.minusDays(3), rewardList.getContent().get(0).getCreated());
+                }
+            }
+        }
+
+        @Nested
+        @DisplayName("find 메소드")
+        class Describe_find {
+
+            @Nested
+            @DisplayName("저장된 리워드 아이디로 조회하면")
+            class Context_saved_rewardId {
+                Reward savedReward;
+
+                @BeforeEach
+                void prepareData() {
+                    savedReward = getRewardCreatedSave(4, givenDefaultReward, givenCreatedDateTime);
+                }
+
+                @Test
+                @DisplayName("리워드 데이타가 리턴된다")
+                void it_returns_reward_list_sort_asc() {
+                    Reward resultReward = getRewardPageSearcher().find(savedReward.getId()).get();
+                    Assertions.assertAll(
+                            "조회된 리워드 내용이 저장한 리워드 내용과 같아야 합니다.",
+                            () -> Assertions.assertEquals(savedReward.getId(), resultReward.getId()),
+                            () -> Assertions.assertEquals(savedReward.getUserNo(), resultReward.getUserNo()),
+                            () -> Assertions.assertEquals(savedReward.getReward(), resultReward.getReward())
+                    );
+                }
+            }
+
+            @Nested
+            @DisplayName("저장되지 않는 리워드 아이디로 조회하면")
+            class Context_not_saved_id {
+                final long givenRewardId = 1233444L;
+
+                @BeforeEach
+                void prepareDAta() {
+                    getRewardDeleteAll();
+                }
+
+                @Test
+                @DisplayName("Optional.empty가 리턴된다")
+                void it_returns_empty() {
+                    Optional<Reward> savedReward = getRewardPageSearcher().find(givenRewardId);
+                    Assertions.assertTrue(savedReward.isEmpty());
                 }
             }
         }
